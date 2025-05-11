72fc5e789cd12a1a4d225c8ce4a8b52eab7668f5
Zaid <iamzaidsheikh@gmail.com>
2025-05-11 14:11:02 +0530

    setup postgres as service database

--- Makefile
+++ Makefile
@@ -1,9 +1,15 @@
-.PHONY: build up makemigrations clean
+.PHONY: build up makemigrations clean build-no-cache up-no-cache
 
 build:
 	@COMPOSE_BAKE=true docker-compose build
 
 up: build
+	@docker-compose up
+
+build-no-cache:
+	@COMPOSE_BAKE=true docker-compose build --no-cache server
+
+up-no-cache: build-no-cache
 	@docker-compose up
 
 makemigrations:
--- config/api.py
+++ config/api.py
@@ -1,17 +1,23 @@
 import logging
 
+from django.db import connections
 from ninja import Router
 
 router = Router()
 logger = logging.getLogger(name="app")
 
 
-@router.get("/health")
+@router.get("/health", response={(200, 503): dict})
 def health_check(request):
     # logger.debug(msg="Debug level log: health endpoint reached")
     # logger.info(msg="Info level log: returning healthy status")
     # logger.warning(msg="Warning level log: example warning in health endpoint")
     # logger.error(msg="Error level log: example error in health endpoint")
     # logger.critical(msg="Critical level log: example critical log in health endpoint")
-
-    return {"status": "healthy"}
+    try:
+        with connections["default"].cursor() as cursor:
+            cursor.execute("SELECT 1;")
+    except Exception as e:
+        logger.error(msg=f"Health check failed: {e}")
+        return 503, {"status": "unhealthy", "reason": "database connection failed"}
+    return 200, {"status": "healthy"}
